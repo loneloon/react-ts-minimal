b74089faac3107e03b2a5923e76d0c48238fedb0
loneloon <f.kleshev@gmail.com>
2023-06-15 09:21:41 +0700

    Template React repository setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { ReactElement} from 'react';
+
+function App(): ReactElement {
+    return (
+        <div>{'Hello World'}</div>
+    );
+}
+
+export default App;
